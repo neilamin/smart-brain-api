385ddd3517de633ef37f9e56fc6cd7425fc0daff
neilamin <neil.k.amin@gmail.com>
2018-10-17 14:57:43 -0400

    fixes

--- controllers/register.js
+++ controllers/register.js
@@ -7,7 +7,7 @@ const handleRegister = (req, res, db, bcrypt) => {
 			db.transaction(trx => {
 				trx.insert({
 					hash: hash,
-					email: email
+					email: email,
 				})
 				.into('login')
 				.returning('email')
